7d9ef7b06f74256d559a6ecbb7a3f9dd88260c31
panevaa <paneva.uk1@gmail.com>
2024-10-31 15:42:00 +0000

    lots of changes. The key one is to merge by contry code not country name

--- prac4test.R
+++ prac4test.R
@@ -4,23 +4,26 @@ library(tidyverse)
 library(tmap)
 library(dplyr)
 library(here)
+library(countrycode)
 
 #read file
-worldmap <- st_read(here("prac4_data", "World_Countries_Generalized.shp"))
-HDRdata <- read_csv(here("prac4_data", "HDR23-24_Composite_indices_complete_time_series.csv"))
+worldmap <- st_read("prac4_data/World_Countries_Generalized.shp")
+HDRdata <- read_csv("prac4_data/HDR23-24_Composite_indices_complete_time_series.csv", locale = locale(encoding = "latin1"))
 
 GII <-HDRdata %>%
   mutate(Difference = gii_2019 - gii_2010)%>%
-  select(country, Difference)
+  filter(!str_detect(iso3, "^ZZ.*")) %>%
+  mutate(iso2c = countrycode(iso3, "iso3c", "iso2c")) %>%
+  select(iso2c, Difference)
 
 slice_head(GII, n=5)
 #join data
 world <- worldmap %>% 
-  merge(., GII, by.x="COUNTRY", by.y="country")
+  merge(., GII, by.x="ISO", by.y="iso2c")
 
 library(tmap)
 library(tmaptools)
-tmap_mode("plot")
+tmap_mode("view")
 tm_shape(world) + 
-  tm_polygons("Difference", palette = "Reds"  , midpoint=0) +
+  tm_polygons("Difference", palette = "-RdBu", midpoint=-0.1, n=5) +
   tm_layout(legend.outside = TRUE)
